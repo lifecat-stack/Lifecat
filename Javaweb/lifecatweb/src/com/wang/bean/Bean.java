fdefcc2c17fce82c083fa37c4058946754c95126
kevinten10 <596823919@qq.com>
2018-08-26 16:54:07 +0800

    v2

--- Javaweb/lifecatweb/src/com/wang/bean/Bean.java
+++
@@ -1,8 +0,0 @@
-package com.wang.bean;
-/**
- * @name Bean
- * @description model返回bean实体
- * @auther ten
- */
-public interface Bean {
-}
--- Javaweb/lifecatweb/src/com/wang/form/MyForm.java
+++
@@ -1,7 +0,0 @@
-package com.wang.form;
-
-import javax.servlet.ServletRequest;
-
-public interface MyForm {
-    public FormResult validate(ServletRequest servletRequest);
-}
