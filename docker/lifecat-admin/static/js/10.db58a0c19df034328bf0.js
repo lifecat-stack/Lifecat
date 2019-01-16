cf9f8c9aa89a6f6cb8d0b043d318485ef69c2b93
kevinten10 <596823919@qq.com>
2019-01-16 14:24:47 +0800

    add docker file

---
+++ docker/lifecat-admin/static/js/10.db58a0c19df034328bf0.js
@@ -0,0 +1,1 @@
+webpackJsonp([10],{gLag:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l={render:function(e,t){var n=t._c;return n("div",{staticStyle:{padding:"30px"}},[n("el-alert",{attrs:{title:"menu 1-2-2",type:"warning",closable:!1}})],1)},staticRenderFns:[]},r=n("VU/8")(null,l,!0,null,null,null);t.default=r.exports}});
---
+++ docker/lifecat-admin/static/js/9.ac3d5f6c690995935577.js
@@ -0,0 +1,1 @@
+webpackJsonp([9],{"v2/0":function(e,t,l){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n={render:function(e,t){var l=t._c;return l("div",{staticStyle:{padding:"30px"}},[l("el-alert",{attrs:{title:"menu 1-3",type:"success",closable:!1}})],1)},staticRenderFns:[]},r=l("VU/8")(null,n,!0,null,null,null);t.default=r.exports}});
